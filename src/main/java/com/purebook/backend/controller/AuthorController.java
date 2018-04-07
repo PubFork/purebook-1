f89a1b46abba7f7abec747ccace8bb70e2c62f94
cHaO5 <quentin.hzk@gmail.com>
2018-04-07 23:21:23 +0800

    Add function find author works

--- src/main/java/com/purebook/backend/controller/AuthorController.java
+++ src/main/java/com/purebook/backend/controller/AuthorController.java
@@ -4,6 +4,8 @@ import com.example.result.JsonResult;
 import com.example.result.JsonResultwithData;
 import com.example.result.ResultCode;
 import com.purebook.backend.entity.Author;
+import com.purebook.backend.entity.Book;
+import com.purebook.backend.entity.BookList;
 import com.purebook.backend.service.AuthorService;
 import com.purebook.backend.util.UnifiedResult;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "v1/author/")
+@RequestMapping(value = "v1/author")
 public class AuthorController {
 
     @Autowired
@@ -35,5 +37,10 @@ public class AuthorController {
         return UnifiedResult.unifiedResult(authorInfo, ResultCode.NOT_FOUND);
 
     }
-    
+
+    @RequestMapping(value = "works", method = RequestMethod.GET)
+    public JsonResult getAuthorWorks(@RequestParam String name) {
+        List<Book> bookLists = authorService.findWorkByNameLike(name);
+        return UnifiedResult.unifiedResult(bookLists, ResultCode.NOT_FOUND);
+    }
 }
--- src/main/java/com/purebook/backend/dao/AuthorRepository.java
+++ src/main/java/com/purebook/backend/dao/AuthorRepository.java
@@ -1,7 +1,9 @@
 package com.purebook.backend.dao;
 
 import com.purebook.backend.entity.Author;
+import com.purebook.backend.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,7 @@ import java.util.List;
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
     List<Author> findByNameLike(String name);
+
+    @Query("select b from Book b, Author a where a.bookId = b.id and a.name like ?1")
+    List<Book> findWorkByNameLike(String name);
 }
